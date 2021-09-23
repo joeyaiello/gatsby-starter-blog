63714ae82247991c944235ab69cb7da6c90f2c8a
joeyaiello <joey.aiello@netlify.com>
2021-09-23 11:48:24 -0700

    take it further

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -28,7 +28,7 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
+          Written by <strong>{get(this, 'props.data.site.siteMetadata.title')}</strong> who lives and works in San
           Francisco building useful things.{' '}
           <a href="https://twitter.com/kylemathews">
             You should follow him on Twitter
