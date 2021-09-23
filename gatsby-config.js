7af3b6c2684216bb58aae345a22f4ab2c15d54b1
joeyaiello <joey.aiello@netlify.com>
2021-09-23 11:20:06 -0700

    test changing author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
+    author: 'Joey Aiello',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
   },
